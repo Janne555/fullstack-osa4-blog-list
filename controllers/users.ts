af75d77e2ccf1299eb34aebfda1e4b4453523bf3
Janne Lavila <janne.lavila@wheelq.com>
2019-10-31 14:04:08 +0200

    add user router

---
+++ controllers/users.ts
@@ -0,0 +1,38 @@
+import { Router } from 'express'
+import User from '../models/user'
+import bcrypt from 'bcrypt'
+
+const userRouter = Router()
+
+userRouter.get('/', async (request, response, next) => {
+  if (!request.body.password || request.body.password.length < 3)
+    return response.status(400).end()
+
+  try {
+    const users = await User.find({})
+    response.json(users.map(u => u.toJSON()))
+  } catch (error) {
+    next(error)
+  }
+})
+
+userRouter.post('/', async (request, response, next) => {
+  try {
+    const body = request.body
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+    const user = new User({
+      username: body.username,
+      name: body.name,
+      passwordHash
+    })
+
+    const result = await user.save()
+    response.json(result.toJSON())
+  } catch (error) {
+    next(error)
+  }
+})
+
+export default userRouter
