13f8fbe76531711b596dad54b00100fb99c8ec61
Janne Lavila <janne.lavila@wheelq.com>
2019-10-31 10:17:26 +0200

    prepare for tests

--- index.ts
+++ index.ts
@@ -1,6 +1,3 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
 import http from 'http'
 import app from './app'
 import { PORT } from './utils/config'
--- utils/config.ts
+++ utils/config.ts
@@ -1,2 +1,8 @@
-export const PORT = process.env.PORT
-export const MONGODB_URI = process.env.MONGODB_URI
+import dotenv from 'dotenv'
+dotenv.config()
+
+export let PORT = process.env.PORT
+export let MONGODB_URI = process.env.MONGODB_URI
+
+if (process.env.NODE_ENV === 'test')
+  MONGODB_URI = process.env.TEST_MONGODB_URI
