8d68e04a8fcde4bcacbf3a3301bdf71b0133b6a0
Janne Lavila <janne.lavila@wheelq.com>
2019-10-31 11:11:22 +0200

    compiled js

--- dist/controllers/blogs.js
+++ dist/controllers/blogs.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,20 +15,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const blog_1 = __importDefault(require("../models/blog"));
 const blogRouter = express_1.Router();
-blogRouter.get('/', (request, response) => {
-    blog_1.default
-        .find({})
-        .then(blogs => {
+blogRouter.get('/', (request, response, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const blogs = yield blog_1.default.find({});
         response.json(blogs);
-    });
-});
-blogRouter.post('/', (request, response) => {
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+blogRouter.post('/', (request, response, next) => __awaiter(void 0, void 0, void 0, function* () {
     const blog = new blog_1.default(request.body);
-    blog
-        .save()
-        .then(result => {
+    try {
+        const result = yield blog.save();
         response.status(201).json(result);
-    });
-});
+    }
+    catch (error) {
+        next(error);
+    }
+}));
 exports.default = blogRouter;
 //# sourceMappingURL=blogs.js.map
--- dist/models/blog.js
+++ dist/models/blog.js
@@ -7,6 +7,13 @@ const blogSchema = new mongoose_1.Schema({
     url: String,
     likes: Number
 });
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    }
+});
 const Blog = mongoose_1.model('Blog', blogSchema);
 exports.default = Blog;
 //# sourceMappingURL=blog.js.map
