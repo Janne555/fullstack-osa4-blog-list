7af8d04166d27947520741f84bc44c35bd1cac42
Janne Lavila <janne.lavila@wheelq.com>
2019-10-31 10:48:14 +0200

    implement 4.8

---
+++ tests/blog_api.test.ts
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose'
+import supertest from 'supertest'
+import app from '../app'
+import Blog from '../models/blog'
+import { blogs } from './mocks/blogs'
+import { IBlog } from '../types'
+
+const api = supertest(app)
+
+beforeAll(async () => {
+  await Blog.deleteMany({})
+  await Promise.all(blogs.map(blog => blog.save()))
+})
+
+afterAll(async () => {
+  await Blog.deleteMany({})
+  mongoose.connection.close()
+})
+
+describe('/api/blog', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blog')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('returns all blogs', async () => {
+    const response = await api.get('/api/blog')
+    expect(response.body.length).toBe(blogs.length)
+  })
+
+  test('a specific blog is within returned blogs', async () => {
+    const response = await api.get('/api/blog')
+    expect(response.body.map((blog: IBlog) => blog.url)).toContainEqual(blogs[0].url)
+  })
+  
+})
+
+
